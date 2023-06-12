655f23d0d0110ddde9c52b2f81cdb715dfaad07b
Mikel Arnaiz <m.arnaiz@catawiki.nl>
2023-06-12 13:57:47 +0200

    Adds paragraph

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,6 +4,7 @@ export default function Page() {
   return (
     <>
       <Header text="Web" />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut fugiat suscipit, neque nam numquam officiis molestiae excepturi harum a assumenda, quaerat aspernatur laborum! Quo possimus repudiandae ea eaque ex? Quis?</p>
       <Button />
     </>
   );

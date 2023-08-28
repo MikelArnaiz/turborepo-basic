fdd8e6aadd0a2549431414b5caffafb2aac332a2
Mikel Arnaiz <m.arnaiz@catawiki.nl>
2023-08-28 11:49:14 +0200

    v7

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -3,5 +3,5 @@
 import * as React from "react";
 
 export const Button = () => {
-  return <button onClick={() => alert("boop")}>Boop v6</button>;
+  return <button onClick={() => alert("boop")}>Boop v7</button>;
 };

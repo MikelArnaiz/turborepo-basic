c327a65ad035bf8053e983f71c7ea8131ff129bb
Mikel Arnaiz <m.arnaiz@catawiki.nl>
2023-12-13 19:35:34 +0100

    uses add function on counter

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,16 @@
 'use client'
 
 import { Button, Header } from 'marnaiz-turborepo-ui'
+import { add } from '@catawiki/poc-math'
+import { useReducer } from 'react'
 
 const onClick = () => {
   alert('you clicked')
 }
 
 export default function Page() {
+  const [counter, increment] = useReducer(add(1), 0)
+
   return (
     <>
       <Header text="this is the apps/web page" />
@@ -15,6 +19,9 @@ export default function Page() {
         molestiae excepturi harum a assumenda, quaerat aspernatur laborum! Quo possimus repudiandae ea eaque ex? Quis?
       </p>
       <Button onClick={onClick} />
+      <hr />
+      <p>Counter: {counter}</p>
+      <button onClick={increment}>Increment</button>
     </>
   )
 }
